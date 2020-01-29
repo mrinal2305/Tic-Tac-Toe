85d99632344ea4c5267b80e568c20f13a46c0010
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-29 23:38:20 +0530

    UI Design Completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,96 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import 'bootstrap/dist/css/bootstrap.css';
+// // Put any other imports below so that CSS from your
+// // components takes precedence over default styles.
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Square extends React.Component {
+  render() {
+    return (
+      <button className="square">
+        {/* TODO */}
+      </button>
+    );
+  }
+}
+
+class Board extends React.Component {
+  renderSquare(i) {
+    return <Square />;
+  }
+ 
+  render() {
+    const status = 'X';
+
+    return (
+      <div class="container">
+        <div className="status row">
+        <code> Next player: </code> <value>{status}</value>
+        </div>
+        <div>
+        <div className="board-row row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+     <div className="container game">
+        <div class="row">
+        <div class="col-3"></div>
+        <div class="col-8">
+
+        <div class="row heading">
+        <p>Tic Tac Toe</p>
+        </div>
+
+        <div class="game-board row">
+          <Board />
+        </div>
+
+        <div class="game-info row">
+            <div>{ /*status*/ }</div>
+            <ol>{ /*TODO*/ }</ol>
+        </div>
+        </div>
+        <div class="col-1"></div>
+      </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
