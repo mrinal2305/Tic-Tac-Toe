6b753e35331383d8c3d84eca7c98f6be5dfd9960
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-01-30 06:03:39 +0530

    Game Created without Time Travel

--- src/index.js
+++ src/index.js
@@ -1,34 +1,78 @@
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import 'bootstrap/dist/css/bootstrap.css';
 // // Put any other imports below so that CSS from your
 // // components takes precedence over default styles.
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square">
-        {/* TODO */}
-      </button>
-    );
-  }
+// class Square extends React.Component {
+
+//   render() {
+//     return (
+//       <button 
+//       className="square" 
+//       onClick={()=>{this.props.onClick()}}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+
+function Square(props) {           // Function Components ,an easy way to write componets
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
+
 class Board extends React.Component {
+  constructor(props){
+    super(props);   //The super keyword is used to access and call functions on an object's parent.
+    this.state = {
+      squares : Array(9).fill(null),
+      xIsNext  : true
+    }
+  }
+
+  handleClick = (i)=>{
+    const squares = this.state.squares.slice(); // we call .slice() to create a copy of the squares array
+    if (calculateWinner(squares) || squares[i]) { // Calculating Winner
+      return; 
+    }
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext : !this.state.xIsNext
+    });
+  }
+
   renderSquare(i) {
-    return <Square />;
+    return <Square 
+    value   = {this.state.squares[i]}
+    onClick = {()=> this.handleClick(i)}
+    />; // Passing value bw components through props
   }
  
   render() {
-    const status = 'X';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    let code;
+    if (winner) {
+      code   = 'Winner: '
+      status = winner;
+    } else {
+      code   = 'Next Player :'
+      status = (this.state.xIsNext ? 'X' : 'O');
+    }
 
     return (
       <div class="container">
         <div className="status row">
-        <code> Next player: </code> <value>{status}</value>
+        <code> {code} </code> <value>{status}</value>
         </div>
         <div>
         <div className="board-row row">
@@ -98,3 +142,23 @@ ReactDOM.render(
 // // unregister() to register() below. Note this comes with some pitfalls.
 // // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
