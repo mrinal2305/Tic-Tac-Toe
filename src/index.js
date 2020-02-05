8b34300f7783011a6a95a4a03de86656e68c0cb8
Mrinal Kumar Karn <mrinalkumarkarn24@gmail.com>
2020-02-05 17:19:00 +0530

    Added onclick ,to generate a button and send key to handleInfo() successfully

--- src/index.js
+++ src/index.js
@@ -2,30 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
+import './index.css';
 
-// // Put any other imports below so that CSS from your
-// // components takes precedence over default styles.
-
-// class Square extends React.Component {
-
-//   render() {
-//     return (
-//       <button 
-//       classNameName="square" 
-//       onClick={()=>{this.props.onClick()}}>
-//         {this.props.value}
-//       </button>
-//     );
-//   }
-// }
 
 function Square(props) {           // Function Components ,an easy way to write componets
 
   var temp = true;
-  temp   = props.value;
-  if(temp){
+  temp = props.value;
+  if (temp) {
     return (
       <button className="square value" onClick={props.onClick}>
         <p>{props.value}</p>
@@ -42,46 +27,16 @@ function Square(props) {           // Function Components ,an easy way to write 
 
 }
 
-
 class Board extends React.Component {
-  // constructor(props){
-  //   super(props);   //The super keyword is used to access and call functions on an object's parent.
-  //   this.state = {
-  //     squares : Array(9).fill(null),
-  //     xIsNext  : true
-  //   }
-  // }
-
-  // handleClick = (i)=>{
-  //   const squares = this.state.squares.slice(); // we call .slice() to create a copy of the squares array
-  //   if (calculateWinner(squares) || squares[i]) { //ignoring a click if someone has won the game or if a Square is already filled 
-  //     return; 
-  //   }
-  //   squares[i] = this.state.xIsNext ? 'X' : 'O';
-  //   this.setState({
-  //     squares: squares,
-  //     xIsNext : !this.state.xIsNext
-  //   });
-  // }
 
   renderSquare(i) {
     return <Square
       value={this.props.squares[i]}
       onClick={() => this.props.onClick(i)}
-    />; // Passing value bw components through props
+    />;
   }
 
   render() {
-    // const winner = calculateWinner(this.state.squares);
-    // let status;
-    // let code;
-    // if (winner) {
-    //   code   = 'Winner: '
-    //   status = winner;
-    // } else {
-    //   code   = 'Next Player :'
-    //   status = (this.state.xIsNext ? 'X' : 'O');
-    // }
 
     return (
       <div className="container">
@@ -107,47 +62,57 @@ class Board extends React.Component {
   }
 }
 
+
+
 class Game extends React.Component {
+
   constructor(props) {
     super(props);
     this.state = {                             // Now all control from Game Component
       history: [{
         squares: Array(9).fill(null),          // Initial all array is null ,showing starting state
-        moves     : 0
+        moves: 0
       }],
-      xIsNext: true
+      xIsNext: true,
     }
   }
 
+
   handleClick = (i) => {
- 
+
     const current = this.state.history[this.state.history.length - 1];
-    console.log(this.state.history);                       
 
     const square = current.squares.slice();                         //saving the history
     if (calculateWinner(square) || square[i]) {             //ignoring a click if someone has won the game or if a Square is already filled 
       return;
-    } 
-     
+    }
+
     square[i] = this.state.xIsNext ? 'X' : 'O';
-    const move = current.moves +1;
+    const move = current.moves + 1;
 
     this.setState({
-      history : this.state.history.concat([{
-        squares:square,
-        moves  :move  
+      history: this.state.history.concat([{
+        squares: square,
+        moves: move
       }]),
       xIsNext: !this.state.xIsNext,       // Changed the state,
     });
-  
+
   }
 
-  handleRestart = ()=>{
-    console.log('Restart Working');
+  handleRestart = () => {
+
+    this.setState({
+      history: [{
+        squares: Array(9).fill(null),          // Initial all array is null ,showing starting state
+        moves: 0
+      }],
+      xIsNext: true
+    })
   }
 
-  handleInfo = ()=>{
-    console.log('Info click working')
+  handleInfo = (data) => {
+    console.log(data);  // consoling the data   
   }
 
   render() {
@@ -155,13 +120,31 @@ class Game extends React.Component {
     const winner = calculateWinner(current.squares);  //Finding winner
     let status;
     let code;
+
+    var items = this.state.history.map((item) => {
+  
+      if(item.moves > 0){
+        return (
+          <li>
+            {/* Nice Logic to send data through handler function */}
+            <button className='button info' onClick={(e) => this.handleInfo(item.moves)} key={item.moves} ><p>Go to #{item.moves}</p></button>
+          </li>
+        ) 
+      }
+      else{
+        return(
+          <div></div>
+        )
+      }
+    })
+
     if (winner) {                   // Declaring Winner
       code = 'Winner: '
       status = winner;
     } else {
       code = 'Next Player :'
       status =
-       (this.state.xIsNext ? 'X' : 'O');
+        (this.state.xIsNext ? 'X' : 'O');
     }
 
     return (
@@ -173,7 +156,6 @@ class Game extends React.Component {
             <div className="row title">
               Tic Tac Toe
         </div>
-
             <div className="row subtitle">
               A Fun Project By <b>Mrinal Kumar Karn</b>
             </div>
@@ -190,13 +172,12 @@ class Game extends React.Component {
                     onClick={this.handleClick} />
                 </div>
               </div>
-              
+
               <div className="col-4 btn-group-vertical game-info">
                 <button className='button restart' onClick={this.handleRestart}><p>Restart</p></button>
-                <button className='button info'    onClick={this.handleInfo}>   <p>Go to 1</p></button>
-                <button className='button info'>   <p>Go to 2</p></button>
-                <button className='button info'>   <p>Go to 3</p></button>
-             </div>
+                {/* <button className='button info' onClick={this.handleInfo}>  <p>Go to 1</p></button> */}
+                {items}
+              </div>
 
             </div>
           </div>
